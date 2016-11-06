361164970703f84ac5228351410895b8705c1e24
Indrek Luuk <indrek.luuk@gmail.com>
2016-11-06 15:42:34 +0200

    alternating monochrome and grayscale lines

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -12,8 +12,7 @@
 #include "Screen.h"
 #include "databuffer/DataBufferSender.h"
 #include "utils/Utils.h"
-
-
+#include "ByteInversionTable.h"
 
 
 Camera camera;
@@ -22,8 +21,9 @@ DataBufferSender dataBufferSender;
 
 
 
-void processFrame();
-
+void processLine(const uint8_t lineIndex);
+void processPixelsGrayscale(const uint8_t lineIndex);
+void processPixelsMonochrome(const uint8_t lineIndex);
 
 
 void run() {
@@ -41,43 +41,28 @@ void run() {
 
   noInterrupts();
   while(true) {
-    processFrame();
+    screen.resetFrame();
+    camera.readFrame(processLine);
   }
 }
-
-
-
-
-void processLine(const uint8_t lineIndex);
-
-void processFrame() {
-
-
-  screen.resetFrame();
-  camera.readFrame(processLine);
-
-  /*
-  Serial.print("Avg: ");
-  Serial.print((int)colorAverage);
-  Serial.print(" Thr: ");
-  Serial.print((int)threshold);
-  Serial.print(" Min: ");
-  Serial.print((int)frameMin);
-  Serial.print(" Max: ");
-  Serial.print((int)frameMax);
-  Serial.println();
-  */
-}
-
-
 
 
 
 void processLine(const uint8_t lineIndex) {
   screen.screenLineStart();
 
+  if (lineIndex & 1) {
+    processPixelsGrayscale(lineIndex);
+  } else {
+    processPixelsMonochrome(lineIndex);
+  }
+
+  screen.screenLineEnd();
+}
+
+
+void processPixelsGrayscale(const uint8_t lineIndex) {
   for (uint8_t i=0; i<camera.getPixelBufferLength(); i++) {
-  //for (uint16_t i=2; i<ll - 2; i+=2) {
     uint8_t greyScale = camera.getPixelByte(i);
 
     screen.sendGrayscalePixelHigh(greyScale);
@@ -99,110 +84,37 @@ void processLine(const uint8_t lineIndex) {
     asm volatile("nop");
     asm volatile("nop");
     asm volatile("nop");
-
   }
+}
 
+void processPixelsMonochrome(const uint8_t lineIndex) {
+  for (uint8_t i=0; i<camera.getPixelBufferLength(); i++) {
+    uint8_t monoChrome = byteInversionTable[camera.getPixelByte(i)] > 0x25 ? 0xFF : 0x00;
 
-  /*
-  //for (uint16_t i=2; i<cameraOV7670.getPixelBufferLength() - 2; i+=4) {
-  for (uint16_t i=2; i<lineLength - 2; i+=2) {
-    uint8_t greyScale = buffer[i];
-    screen.sendPixelByte(graysScaleTableHigh[greyScale]);
-    lineTotal += greyScale;
-    if (greyScale > rowMax) rowMax = greyScale;
+    screen.sendPixelByte(monoChrome);
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+    asm volatile("nop");
+
+    screen.sendPixelByte(monoChrome);
     //asm volatile("nop");
 
-    screen.sendPixelByte(graysScaleTableLow[greyScale]);
-    if (greyScale < rowMin) rowMin = greyScale;
-    //asm volatile("nop");
   }
-  colorTotal += lineTotal;
-  if (rowMax > frameMax) frameMax = rowMax;
-  if (rowMin < frameMin) frameMin = rowMin;
-  */
-
-
-/*
-  // screen greyscale/monochrome divider
-  sendPixelByte(0);
-  fullSendPixelDelay();
-  sendPixelByte(0);
-  fullSendPixelDelay();
-
-
-
-  // process and display monochrome
-  for (uint16_t i=2; i<cameraOV7670.getPixelBufferLength(); i+=8) {
-    uint8_t monoChrome = byteInversionTable[cameraOV7670.getPixelByte(i)] > threshold ? 0xFF : 0x00;
-
-    sendPixelByte(monoChrome);
-
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-
-    sendPixelByte(monoChrome);
-
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-
-    sendPixelByte(monoChrome);
-
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-
-    sendPixelByte(monoChrome);
-
-    asm volatile("nop");
-
-  }
-
-*/
-  screen.screenLineEnd();
 }
 
 
