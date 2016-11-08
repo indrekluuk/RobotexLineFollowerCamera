b4becef37cd8b6bf2982d0b5f56652434836550a
Indrek Luuk <indrek.luuk@gmail.com>
2016-11-08 23:39:45 +0200

    working solution

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -23,7 +23,8 @@ DataBufferSender dataBufferSender;
 
 void processLine(const uint8_t lineIndex);
 void processPixelsGrayscale();
-void processPixelsMonochrome(bool horozonatlLine);
+void processPixelsMonochrome(bool horizontalLine);
+uint8_t getLine();
 
 
 
@@ -39,42 +40,53 @@ void run() {
 }
 
 
+
+
 uint8_t threshold = 0x80;
+uint8_t scanLine = 30;
 const uint8_t monochromeLineLength = 80;
 uint8_t monochromeLine[monochromeLineLength];
-union {
-    struct {
-        int8_t right;
-        int8_t left;
-    };
-    uint8_t data[2];
-} line;
+int8_t lineRight;
+int8_t lineLeft;
 
 
 
 void processLine(const uint8_t lineIndex) {
-  line.right = -1;
-  line.left = 0x1F;
+  lineRight = -1;
+  lineLeft = monochromeLineLength;
 
   screen.screenLineStart(lineIndex);
   processPixelsGrayscale();
-  processPixelsMonochrome(lineIndex == 30);
+  processPixelsMonochrome(lineIndex == scanLine);
   screen.screenLineEnd();
 
-  if (lineIndex == 30){
-    dataBufferSender.sendMessage(line.data, 2);
+  if (lineIndex == scanLine){
+    uint8_t aa[3];
+    uint8_t line = getLine();
+    aa[0] = line;
+    aa[1] = ((uint8_t)lineRight) & 0x7F;
+    aa[2] = ((uint8_t)lineLeft) & 0x7F;
+    dataBufferSender.sendMessage(aa, 3);
   }
 }
 
 
+uint8_t getLine() {
+  int8_t line = (40 - lineRight) < (lineLeft - 40) ? lineRight : lineLeft;
+  return line < 0 || line >= monochromeLineLength ? 0x7F : static_cast<uint8_t >(line);
+}
+
 
 void processPixelsGrayscale() {
+  uint8_t lineMin= 0xFF;
+
   for (uint8_t i=0; i<camera.getPixelBufferLength(); i++) {
     uint8_t pixelByte = camera.getPixelByte(i);
 
     screen.sendGrayscalePixelHigh(pixelByte);
 
-    monochromeLine[i] = byteInversionTable[pixelByte] > threshold ? 0x00 : 0xFF;
+    uint8_t correctedPixel = byteInversionTable[pixelByte];
+    monochromeLine[i] = correctedPixel > threshold ? 0x00 : 0xFF;
     /*
     asm volatile("nop");
     asm volatile("nop");
@@ -91,24 +103,30 @@ void processPixelsGrayscale() {
      */
 
     screen.sendGrayscalePixelLow(pixelByte);
+
+    if (lineMin > correctedPixel) lineMin = correctedPixel;
+    asm volatile("nop");
+    /*
     asm volatile("nop");
     asm volatile("nop");
     asm volatile("nop");
-    asm volatile("nop");
+     */
   }
+
+  threshold = lineMin + 20;
 }
 
 
 
 
-void processPixelsMonochrome(bool horozonatlLine) {
+void processPixelsMonochrome(bool horizontalLine) {
   for (uint8_t i=0; i<monochromeLineLength/2; i++) {
     uint8_t byte = monochromeLine[i];
 
     screen.sendPixelByte(byte);
 
     if (byte) {
-      line.right = i;
+      lineRight = i;
     }
 
     asm volatile("nop");
@@ -129,7 +147,7 @@ void processPixelsMonochrome(bool horozonatlLine) {
     asm volatile("nop");
     asm volatile("nop");
 
-    screen.sendPixelByte(horozonatlLine?0xFF:0);
+    screen.sendPixelByte(horizontalLine?0xFF:0);
     asm volatile("nop");
     asm volatile("nop");
     asm volatile("nop");
@@ -147,8 +165,8 @@ void processPixelsMonochrome(bool horozonatlLine) {
 
     screen.sendPixelByte(monochromeLine[i]);
 
-    if (byte && line.left >= monochromeLineLength) {
-      line.left = i;
+    if (byte && (lineLeft >= monochromeLineLength)) {
+      lineLeft = i;
     }
 
     asm volatile("nop");
@@ -169,7 +187,7 @@ void processPixelsMonochrome(bool horozonatlLine) {
     //asm volatile("nop");
     //asm volatile("nop");
 
-    screen.sendPixelByte(horozonatlLine?0xFF:0);
+    screen.sendPixelByte(horizontalLine?0xFF:0);
     asm volatile("nop");
     asm volatile("nop");
     asm volatile("nop");
