6c772afcd287817dc2b68f264706c82b3244eaee
Indrek Luuk <indrek.luuk@gmail.com>
2016-12-01 13:46:14 +0200

    process line split

--- arduino/src/line/Line.h
+++ arduino/src/line/Line.h
@@ -15,17 +15,25 @@
 template <int8_t totalRowCount>
 class Line {
 
-    static const int8_t turnDetectionFromLine = 5;
-    static const int8_t lineEnd = -1;
-    static const int8_t lineTurn = -2;
+    static const int8_t ignoreTurnDetectionToLine = 50;
+    static const int8_t splitDetectionToLine = 60;
+    static const int8_t splitDetectionMinimumLineCount = 10;
+    static const int8_t lineTurn = -1;
 
     RowBitmapLineSegmentFinder bitmapLineFinder;
     LineEdge startEdge;
     LineEdge endEdge;
-    int8_t previousDetectedLinePosition;
+
+    int8_t previousLinePosition;
+    int8_t previousAltLine1Position;
+    int8_t previousAltLine2Position;
 
     int8_t lineBottomRowIndex;
     int8_t lineBottomPosition;
+    int8_t lineFirstTurnRowIndex;
+    int8_t lineFirstTurnPosition;
+    int8_t lineTopCandidateRowIndex;
+    int8_t lineTopCandidatePosition;
     int8_t lineTopRowIndex;
     int8_t lineTopPosition;
     bool isEndOfLine;
@@ -58,9 +66,12 @@ public:
 
 
 private:
-    int8_t updateLine(uint8_t rowIndex, LineSegment &lineSegment);
+    int8_t updateLine(LineSegment &lineSegment);
+    inline bool isLastRow(int8_t rowIndex) __attribute__((always_inline));
     inline void setLineBottom(uint8_t rowIndex, int8_t position) __attribute__((always_inline));
-    inline void setLineTop(uint8_t rowIndex, int8_t position, bool isEnd) __attribute__((always_inline));
+    inline void setLineFirstTurn(uint8_t rowIndex, int8_t position) __attribute__((always_inline));
+    inline void setLineTopCandiate(uint8_t rowIndex, int8_t position) __attribute__((always_inline));
+    inline void setLineTop(uint8_t rowIndex, int8_t linePosition, int8_t altLine1Position, int8_t altLine2Position, bool isEnd) __attribute__((always_inline));
 };
 
 
@@ -80,10 +91,16 @@ void Line<totalRowCount>::resetLine() {
   startEdge.reset();
   endEdge.reset();
 
-  previousDetectedLinePosition = LineSegment::rowRangeMidPoint;
+  previousLinePosition = LineSegment::rowRangeMidPoint;
+  previousAltLine1Position = LineSegment::lineNotFound;
+  previousAltLine2Position = LineSegment::lineNotFound;
 
   lineBottomRowIndex = -1;
   lineBottomPosition = -1;
+  lineFirstTurnRowIndex = -1;
+  lineFirstTurnPosition = -1;
+  lineTopCandidateRowIndex = -1;
+  lineTopCandidatePosition = -1;
   lineTopRowIndex = -1;
   lineTopPosition = -1;
   isEndOfLine = false;
@@ -95,34 +112,70 @@ void Line<totalRowCount>::setRowBitmap(uint8_t rowIndex, uint8_t bitmapHigh, uin
 
   if (lineTopRowIndex <0) {
     bitmapLineFinder.nextRow(rowIndex, bitmapHigh, bitmapLow);
-    int8_t currentDetectedLinePosition = LineSegment::lineNotFound;
+    int8_t currentLinePosition = LineSegment::lineNotFound;
+    int8_t currentAltLine1Position = LineSegment::lineNotFound;
+    int8_t currentAltLine2Position = LineSegment::lineNotFound;
 
     if (!bitmapLineFinder.isLineFound()) {
       if (lineBottomRowIndex >= 0) {
-        setLineTop(rowIndex-1, previousDetectedLinePosition, true);
+        setLineTop(rowIndex - 1, previousLinePosition, previousAltLine1Position, previousAltLine2Position, true);
+      }
+    } else if (bitmapLineFinder.isLineSplit()) {
+      // if line split then do nothing until one of the forks has ended
+      currentAltLine1Position = bitmapLineFinder.getFirstLine().getLinePosition();
+      currentAltLine2Position = bitmapLineFinder.getFirstLine().getLinePosition();
+      if (isLastRow(rowIndex)) {
+        setLineTop(rowIndex, LineSegment::lineNotFound, currentAltLine1Position, currentAltLine2Position, true);
       }
     } else {
-      LineSegment &lineSegment = bitmapLineFinder.getFirstLine();
+      LineSegment &lineSegment = bitmapLineFinder.getSingleLine();
 
-      if (lineBottomRowIndex < 0) {
-        setLineBottom(rowIndex, lineSegment.getLinePosition());
-      }
+      if (lineTopCandidatePosition >= 0) {
+        // top line candidate already. check some additional lines to check for possible splits.
+        if ((rowIndex >= splitDetectionToLine)
+            && (rowIndex >= (lineTopCandidateRowIndex + splitDetectionMinimumLineCount))) {
 
-      //currentDetectedLinePosition = updateLine(rowIndex, lineSegment);
-      currentDetectedLinePosition = lineSegment.getLinePosition();
+          if (previousLinePosition < 0) {
+            // if no previous line position then there was a split
+            setLineTop(rowIndex, lineSegment.getLinePosition(), LineSegment::lineNotFound, LineSegment::lineNotFound, false);
+          } else {
+            // if previous position was not split then use candidate as line top
+            setLineTop(lineTopCandidateRowIndex, lineTopCandidatePosition, LineSegment::lineNotFound, LineSegment::lineNotFound, false);
+          }
+        }
 
-      if (currentDetectedLinePosition == lineTurn) {
-        setLineTop(rowIndex - 1, previousDetectedLinePosition, false);
-      } else if (currentDetectedLinePosition == lineEnd) {
-        setLineTop(rowIndex - 1, previousDetectedLinePosition, true);
       } else {
-        if (rowIndex == totalRowCount - 1) {
-          setLineTop(rowIndex, currentDetectedLinePosition, true);
+        // if possible line top not found yet then look for line top
+
+        if (lineBottomRowIndex < 0) {
+          setLineBottom(rowIndex, lineSegment.getLinePosition());
+        }
+
+        currentLinePosition = updateLine(lineSegment);
+
+        if (currentLinePosition == lineTurn) {
+          if (rowIndex < ignoreTurnDetectionToLine) {
+            // if in the ignore turn zone then just store the point as first turn
+            if (lineFirstTurnRowIndex < 0) {
+              setLineFirstTurn(rowIndex, previousLinePosition);
+            }
+          } else {
+            // turn after ignoring zone is line top candidate. Continue a little further to check for line split
+            setLineTopCandiate(rowIndex - 1, previousLinePosition);
+          }
+          currentLinePosition = lineSegment.getLinePosition();
+        } else {
+          // reached top of the screen
+          if (isLastRow(rowIndex)) {
+            setLineTop(rowIndex, currentLinePosition, LineSegment::lineNotFound, LineSegment::lineNotFound, true);
+          }
         }
       }
     }
 
-    previousDetectedLinePosition = currentDetectedLinePosition;
+    previousLinePosition = currentLinePosition;
+    previousAltLine1Position = currentAltLine1Position;
+    previousAltLine2Position = currentAltLine2Position;
   }
 }
 
@@ -130,56 +183,40 @@ void Line<totalRowCount>::setRowBitmap(uint8_t rowIndex, uint8_t bitmapHigh, uin
 
 
 template <int8_t totalRowCount>
-int8_t Line<totalRowCount>::updateLine(uint8_t rowIndex, LineSegment &lineSegment) {
-  if (bitmapLineFinder.isLineFound()) {
-    return lineEnd;
-  } else {
-    return lineSegment.getLinePosition();
-  }
-
-
-
-  /*
-
+int8_t Line<totalRowCount>::updateLine(LineSegment &lineSegment) {
   if (!startEdge.isInitialized()) {
     startEdge.init(lineSegment.getStart(), lineSegment.getLinePosition());
     endEdge.init(lineSegment.getEnd(),lineSegment.getLinePosition());
     return lineSegment.getLinePosition();
   } else {
-    if (lineSegment.touchesSegment(startEdge.currentStepPosition, endEdge.currentStepPosition)) {
 
-      if (rowIndex < turnDetectionFromLine) {
-        startEdge.resetFirstStepTo(lineSegment.getStart(), lineSegment.getLinePosition());
-        endEdge.resetFirstStepTo(lineSegment.getEnd(), lineSegment.getLinePosition());
-        return lineSegment.getLinePosition();
-      }
+    startEdge.update(lineSegment.getStart());
+    endEdge.update(lineSegment.getEnd());
 
-      startEdge.update(lineSegment.getStart());
-      endEdge.update(lineSegment.getEnd());
-
-      if (startEdge.isContinues() && endEdge.isContinues()) {
-        startEdge.calculateLinePositionToEdge(lineSegment.getLinePosition());
-        endEdge.calculateLinePositionToEdge(lineSegment.getLinePosition());
-        return lineSegment.getLinePosition();
-      } else if (startEdge.isContinues()) {
-        startEdge.calculateLinePositionToEdgeDecreaseOnly(lineSegment.getLinePosition());
-        return startEdge.getLinePositionFromEdge();
-      } else if (endEdge.isContinues()) {
-        endEdge.calculateLinePositionToEdgeDecreaseOnly(lineSegment.getLinePosition());
-        return endEdge.getLinePositionFromEdge();
-      } else {
-        return lineTurn;
-      }
-
+    if (startEdge.isContinues() && endEdge.isContinues()) {
+      startEdge.calculateLinePositionToEdge(lineSegment.getLinePosition());
+      endEdge.calculateLinePositionToEdge(lineSegment.getLinePosition());
+      return lineSegment.getLinePosition();
+    } else if (startEdge.isContinues()) {
+      startEdge.calculateLinePositionToEdgeDecreaseOnly(lineSegment.getLinePosition());
+      return startEdge.getLinePositionFromEdge();
+    } else if (endEdge.isContinues()) {
+      endEdge.calculateLinePositionToEdgeDecreaseOnly(lineSegment.getLinePosition());
+      return endEdge.getLinePositionFromEdge();
     } else {
-      return lineEnd;
+      startEdge.resetFirstStepTo(lineSegment.getStart(), lineSegment.getLinePosition());
+      endEdge.resetFirstStepTo(lineSegment.getEnd(), lineSegment.getLinePosition());
+      return lineTurn;
     }
   }
-   */
 }
 
 
 
+template <int8_t totalRowCount>
+bool Line<totalRowCount>::isLastRow(int8_t rowIndex) {
+  return rowIndex == totalRowCount - 1;
+}
 
 
 
@@ -220,9 +257,32 @@ void Line<totalRowCount>::setLineBottom(uint8_t rowIndex, int8_t position) {
 
 
 template <int8_t totalRowCount>
-void Line<totalRowCount>::setLineTop(uint8_t rowIndex, int8_t position, bool isEnd) {
+void Line<totalRowCount>::setLineFirstTurn(uint8_t rowIndex, int8_t position) {
+  lineFirstTurnRowIndex = rowIndex;
+  lineFirstTurnPosition = position;
+}
+
+
+template <int8_t totalRowCount>
+void Line<totalRowCount>::setLineTopCandiate(uint8_t rowIndex, int8_t position) {
+  lineTopCandidateRowIndex = rowIndex;
+  lineTopCandidatePosition = position;
+}
+
+
+template <int8_t totalRowCount>
+void Line<totalRowCount>::setLineTop(uint8_t rowIndex, int8_t linePosition, int8_t altLine1Position, int8_t altLine2Position, bool isEnd) {
   lineTopRowIndex = rowIndex;
-  lineTopPosition = position;
+  if (linePosition != LineSegment::lineNotFound) {
+    lineTopPosition = linePosition;
+  } else if (altLine1Position != LineSegment::lineNotFound && altLine2Position != LineSegment::lineNotFound) {
+    lineTopPosition = abs(altLine1Position - LineSegment::rowRangeMidPoint)
+                      < abs(altLine2Position - LineSegment::rowRangeMidPoint) ?
+                      altLine1Position : altLine2Position;
+  } else {
+    lineTopPosition = LineSegment::rowRangeMidPoint;
+  }
+
   isEndOfLine = isEnd;
 }
 
